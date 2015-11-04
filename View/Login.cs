be4eaac468475a96a8bd8aba27934c6c4da26b8f
janekbaraniewski <janekbaraniewski@gmail.com>
2015-11-04 21:45:45 +0100

    Login window added vol2

---
+++ View/Login.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace View
+{
+    public partial class LoginForm : Form
+    {
+        private string login = "admin";
+        private string password = "admin";
+
+
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (loginBox.Text == login && passwordBox.Text == password)
+            {
+                this.Hide();
+                new Form1().Show();
+            }
+            else
+            {
+                MessageBox.Show("Authentication failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+       
+    }
+}
