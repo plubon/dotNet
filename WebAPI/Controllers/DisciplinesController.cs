219572b75f4e2e4dcd045d6eb79d92ce132d3ae5
janekbaraniewski@gmail.com <Narkotyki997>
2015-12-07 17:23:41 +0100

    Added files for rest of the controllers

---
+++ WebAPI/Controllers/DisciplinesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Model.Enitites;
+using Repository;
+
+namespace WebAPI.Controllers
+{
+    [RoutePrefix("api/disciplines")]
+    public class DisciplinesController : ApiController
+    {
+        readonly DisciplineRepository _repository = new DisciplineRepository();
+
+        [Route("")]
+        public IEnumerable<Discipline> GetAllLeagues()
+        {
+            return _repository.GetAll();
+        }
+    }
+}
